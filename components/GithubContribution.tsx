6f7225b847f89ca6da20677898f2d8cb922d0752
Alwaz <alwazkazi3@gmail.com>
2024-09-11 00:40:29 +0500

    chore: updated github username

--- components/GithubContribution.tsx
+++ components/GithubContribution.tsx
@@ -1,15 +1,19 @@
 "use client";
 
-import GitHubCalendar from 'react-github-calendar';
-import SectionContainer from './SectionContainer';
+import GitHubCalendar from "react-github-calendar";
+import SectionContainer from "./SectionContainer";
 
-export default function GithubContribution
-() {
+export default function GithubContribution() {
   return (
     <SectionContainer>
-        <div className='flex justify-center items-center mt-10 w-full'>
-          <GitHubCalendar username="MohamedBoukthir" fontSize={17} blockRadius={2} year={new Date().getFullYear()}/>
-        </div>
+      <div className="flex justify-center items-center mt-10 w-full">
+        <GitHubCalendar
+          username="Alwaz"
+          fontSize={17}
+          blockRadius={2}
+          year={new Date().getFullYear()}
+        />
+      </div>
     </SectionContainer>
-  )
+  );
 }
