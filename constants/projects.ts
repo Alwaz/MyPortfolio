8d7f326f661cc8da76bb918872b36282e5b21651
Alwaz <alwazkazi3@gmail.com>
2024-12-05 23:37:47 +0500

    feat(projects): added another project.

--- constants/projects.ts
+++ constants/projects.ts
@@ -1,6 +1,6 @@
 import { Project } from "@/Interface/projects";
 import SS from "../public/selectsuite.png";
-import Swiftwheels from "../public/swiftwheels.png";
+import SAAS from "../public/landing-page.png";
 
 export const projects: Project[] = [
   {
@@ -24,27 +24,17 @@ export const projects: Project[] = [
     url: "https://www.selectsuite.net",
     repository: "",
   },
-  // {
-  //   slug: "swiftwheels",
-  //   title: "SwiftWheels",
-  //   brief:
-  //     "Redefining car rentals with a dynamic web app that offers seamless booking, real-time availability, and an intuitive user interface.",
-  //   description:
-  //     "SwiftWheels is a powerful Car Rental Project! I'll show you how to build a dynamic web app with Spring Boot, Angular, daisy UI, tailwindCSS, and MySQL.",
-  //   image: Swiftwheels,
-  //   stack: [
-  //     "Java",
-  //     "Spring Boot",
-  //     "Spring Security",
-  //     "Spring Data JPA",
-  //     "TypeScript",
-  //     "MySQL",
-  //     "Angular",
-  //     "Tailwind CSS",
-  //   ],
-  //   url: "",
-  //   repository: "https://github.com/MohamedBoukthir/Car_Rental",
-  // },
+  {
+    slug: "saas-animated-landing-page",
+    title: "Saas landing page",
+    brief: "An animated Saas landing page using NextJs and framer motion.",
+    description:
+      "An animated Saas landing page using NextJs, tailwindCSS and framer motion.",
+    image: SAAS,
+    stack: ["NextJs", "Typescript", "Framer motion", "Tailwind CSS"],
+    url: "https://saas-landing-page-sigma-pink.vercel.app/",
+    repository: "https://github.com/Alwaz/saas-landing-page",
+  },
   // {
   //   slug: "wave",
   //   title: "Wave",
